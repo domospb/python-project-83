4885a773bc9e0510dcb4d18f3e6a0cd97c68e100
DomoArty <devdomo@DiLong>
2024-08-24 20:28:34 +0300

    edit database.sql to fit both PostgreSQL and SQLite

--- database.sql
+++ database.sql
@@ -1,7 +1,9 @@
+-- Create the urls table if it doesn't exist
 CREATE TABLE IF NOT EXISTS urls (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create a unique index on the name column
 CREATE UNIQUE INDEX IF NOT EXISTS urls_name_idx ON urls (name);
